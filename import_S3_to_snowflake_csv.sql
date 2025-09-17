513e4a6905eb23182cc6d48c11c602c607e852dd
kalyan-sudo <kalyan@utg.io>
2025-09-16 22:56:30 -0700

    Add files via upload

---
+++ import_S3_to_snowflake_csv.sql
@@ -0,0 +1,107 @@
+macros/import_S3_to_snowflake_csv.sql
+=====================================
+
+{% macro import_s3_to_snowflake_csv(bucket,folder,pattern,model,table,file_format={},copy_options={}) %}
+{# 
+    bucket:
+        s3 url, eg. s3://<bucket_name>-<domain>
+    folder:
+        s3 prefix/path for files, eg. <folder>/<subfolder>
+    pattern:
+        regex for files on s3 to ingest
+            examples:
+                '.*'        all files with prefix defined in "folder"
+                '.*[.]csv'  all files with extension '.csv' and prefix defined in "folder"
+    model:
+        use "this" keyword to pass in model relation object, database and schema for model relation will be used
+            eg. if environment is PRD and model path = 'models/WMHS_CONTRACTS/STG/...',
+                then: database = 'PRD_WMHS_CONTRACTS', schema = 'STG'
+    table:
+        string value for name of table where data from s3 should be copied to
+            !! columns in table should match the columns in the s3 file EXACTLY !!
+    lnd_insert:
+        when true, calls macro 'stage_to_land.sql' -- assumes table of the same name as 'table' exists in the LND schema
+        and has all the same columns as well as a column to store timestamp at time of insert (see timestamp_column).
+        eg. if domain = 'PRD', model = 'models/WMHS_CONTRACTS/STG/...', and table = 'cde_data_wmhs':
+            All data from PRD_WMHS_CONTRACTS.STG.cde_data_wmhsE will be inserted into PRD_WMHS_CONTRACTS.LND.cde_data_wmhs
+            if LND table does not have all the columns represented in STG table, this will fail.
+        default = false
+    timestamp_column:
+        string value representing name of timestamp column in table in the LND schema (see lnd_insert above)
+        default = 'AUDIT_LOAD_DTM'
+    file_format={}:
+        dictionary object for file format values. Currently only supports NULL_IF values
+        
+        'null_if': string or list of string values to convert to null during ingestion
+            eg. file_format={'null_if':''} -- fields with blank values will be converted to null during ingestion
+            eg. file_format={'null_if':['','na','empty']} -- fields with blank values, or 'na' or 'empty' will be converted to null during ingestion
+            when 'null_if' key is not present in file_format dictionary, the 'NULL_IF' line is omitted from 'FILE_FORMAT' under the copy into statement
+                the snowflake default in this case will be '\N' -- See https://docs.snowflake.com/en/sql-reference/sql/copy-into-table
+
+#}
+
+    {% set location = 's3://{}/{}/'.format(bucket,folder) %}
+    {% set database = model.database %}
+    {% set schema = model.schema %}
+
+    {% set null_if = '' %}
+    {% if file_format.get('null_if', true) == false %}
+        {%- set null_if = '' -%}
+    {% elif file_format.get('null_if') is string %}
+        {% set null_if = 'NULL_IF = ' ~ "'" ~ file_format.get('null_if') ~ "'," %}
+    {% elif file_format.get('null_if') is iterable %}
+        {% set null_conditions = [] %}
+        {% for condition in file_format.get('null_if') %}
+            {% do null_conditions.append("'" ~ condition ~ "'") %}
+        {% endfor %}
+        {% set null_if = 'NULL_IF = (' ~ null_conditions | join(', ') ~ '),' %}
+    {% elif not file_format.get('null_if') %}
+        {% set null_if = '' %}
+    {% else %}
+        {{ exceptions.raise_compiler_error("Invalid argument type: expected string or list of strings.") }}
+    {% endif %}
+
+{% set on_error = 'ABORT_STATEMENT' %}
+    {% if copy_options.get('on_error', true) == false %}
+        {%- set on_error = 'ABORT_STATEMENT' -%}
+    {% elif copy_options.get('on_error') is string %}
+        {% set on_error = copy_options.get('on_error') %}
+    {% elif not copy_options.get('on_error') %}
+        {% set on_error = 'ABORT_STATEMENT' %}
+    {% else %}
+        {{ exceptions.raise_compiler_error("Invalid argument type: expected string") }}
+    {% endif %}
+
+
+
+    {% set copy_into_query %}
+
+        TRUNCATE TABLE {{database}}.{{schema}}.{{table}}; {# Clear staging table before copy #}
+
+        COPY INTO {{database}}.{{schema}}.{{table}}
+        FROM {{location}} 
+        storage_integration = {{ env_var("DBT_S3_TO_SNOWFLAKE_INT") }}
+        PATTERN = '{{ pattern }}'
+        FILE_FORMAT = (
+            TYPE=csv
+            COMPRESSION=AUTO,
+            FIELD_DELIMITER = ',',
+            SKIP_HEADER=1,
+            SKIP_BLANK_LINES=TRUE,
+            TRIM_SPACE=TRUE,
+            FIELD_OPTIONALLY_ENCLOSED_BY='"',
+            {{ null_if }} {# whole line is omitted if no values present for null_if conditions #}
+            ERROR_ON_COLUMN_COUNT_MISMATCH=TRUE,
+            REPLACE_INVALID_CHARACTERS=TRUE,
+            EMPTY_FIELD_AS_NULL=TRUE
+        )
+        ON_ERROR='{{ on_error }}'
+        PURGE=FALSE
+        TRUNCATECOLUMNS=FALSE
+        FORCE=FALSE
+      
+
+    {% endset %}
+    {{copy_into_query}}
+
+{% endmacro %}
---
+++ load_successfactors_to_snowflake.sql
@@ -0,0 +1,28 @@
+models/wmhs/STG/load_successfactors_to_snowflake.sql
+=====================================================
+
+{% set aws_domain = "wmoraclecloud-" ~ env_var("DBT_AWS_DOMAIN") %}
+
+{{
+    config(
+        materialized="table",
+        pre_hook=[
+            import_s3_to_snowflake_csv(
+                aws_domain,                
+				"HCM/DEV3/WMHS_HCM/INB/PRE/SUCCESSFACTORS",
+                '.*[.]csv',
+                this,
+                "STG_WMHS_SUCCESSFACTORS_DETAILS",
+                file_format={
+                    "null_if": ["(None identified)", "", "(None Identified)"]
+                },
+                copy_options={"on_error": "SKIP_FILE"},
+            )
+        ],
+        post_hook=["DROP TABLE IF EXISTS load_stg_wmhs_successfactors_details"],
+    )
+}}
+
+
+select count(*) as row_count
+from {{ source('wmhs_stg', 'STG_WMHS_SUCCESSFACTORS_DETAILS') }}
